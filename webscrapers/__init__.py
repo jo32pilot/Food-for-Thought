25a778210cabe0b045dfbd98192b00e6dbc3e6c2
John Li <john166@rocketmail.com>
2020-04-19 15:41:15 -0700

    starting webscrapers

---
+++ webscrapers/__init__.py
@@ -0,0 +1,3 @@
+from .scraper_base import ScraperBase
+from .foodnetwork import FoodNetwork
+
---
+++ webscrapers/tests/test_base.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('/home/jo32pilot/cse110/')
+
+import webscrapers
+
+scraper = webscrapers.FoodNetwork()
+scraper.parse()
+scraper._print_links()
+scraper.scrape()
+scraper._print_recipes()
