284f6b8f080ee70b44439fb6c4141dcab092a196
landreavega <lacevedo@ucsd.edu>
2020-05-10 18:00:21 -0700

    Added comments to code

--- FoodForThought/app/src/main/java/com/example/foodforthought/MainFragment.java
+++ FoodForThought/app/src/main/java/com/example/foodforthought/MainFragment.java
@@ -16,15 +16,18 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.google.android.material.navigation.NavigationView;
 
+// Fragment to load the main feed fragment
 public class MainFragment extends Fragment {
 
     @Nullable
     @Override
+    // Initialize view object with layout related to the main feed
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         return view;
     }
-
+    // Method to initialize subclasses
+    // Future implementation will include onClickListeners to navigate between fragments in other ways
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
--- FoodForThought/app/src/main/java/com/example/foodforthought/ShoppingFragment.java
+++ FoodForThought/app/src/main/java/com/example/foodforthought/ShoppingFragment.java
@@ -9,14 +9,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+// Fragment to load the shopping fragment
 public class ShoppingFragment extends Fragment {
     @Nullable
     @Override
+    // Initializes view object with the layout associated with shopping
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_shopping, container, false);
         return view;
     }
-
+    // Method to initialize subclasses
+    // Future implementation will include onClickListeners to navigate between fragments in other ways
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
