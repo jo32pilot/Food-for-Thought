4225662610992167b26f371bafca2aa89f0d4cbc
Johnathan Tran <j4tran@ucsd.edu>
2020-05-30 17:07:53 -0700

    Updating username to display

--- FoodForThought/app/src/main/java/com/example/foodforthought/UserProfileFragment.java
+++ FoodForThought/app/src/main/java/com/example/foodforthought/UserProfileFragment.java
@@ -4,22 +4,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.foodforthought.Misc.Database;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 // Fragment to load the user profile fragment
 public class UserProfileFragment extends Fragment {
+    private TextView name;
+    private Database db = new Database();
+    private View view;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_userprofile, container, false);
+        view = inflater.inflate(R.layout.fragment_userprofile, container, false);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        String uid = user.getUid();
+        name = view.findViewById(R.id.username);
+
         return view;
     }
+
+    OnCompleteListener onGetName = new OnCompleteListener() {
+        @Override
+        public void onComplete(@NonNull Task task) {
+
+        }
+    };
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
