8c1a5510f8e073f87e0569fb52045ee228dbdb0b
Ankur Duggal <ankurduggal@ankurs-mbp.lan>
2020-06-04 12:11:29 -0700

    Sign up link looks like link

--- FoodForThought/app/src/main/java/com/example/foodforthought/Controller/SignUpFragment.java
+++ FoodForThought/app/src/main/java/com/example/foodforthought/Controller/SignUpFragment.java
@@ -41,7 +41,7 @@ public class SignUpFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.signup_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_signup, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
